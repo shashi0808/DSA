250b97a6e2d60b302e034ee72547a00adff1d5a2
shashi0808 <96880312+shashi0808@users.noreply.github.com>
2022-02-22 20:38:21 +0530

    Added watermelon codeforces

---
+++ watermelonCodeforces.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ int w;
+ cin>>w;
+ 
+ if(w%2==0){
+     if(w==2 || w==0){
+     cout<<"NO";
+ }else{
+     cout<<"YES";
+ }
+ }
+ else{
+     cout<<"NO";
+ }
+ 
+	return 0;
+}
