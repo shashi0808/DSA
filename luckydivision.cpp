29fd3a52e9f0979b8eaea6e144e25d920f20d35d
shashi0808 <96880312+shashi0808@users.noreply.github.com>
2022-02-22 22:04:51 +0530

    added lucky division

---
+++ luckydivision.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int s;
+    cin>>s;
+    if(s%4==0 || s%7==0 || s%47==0 || s%477==0 || s%474==0 || s%744==0 ||s%747==0 || s%74==0 ){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
