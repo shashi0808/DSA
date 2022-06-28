837a23df762bf5395c4a6b2752d64d9f58c2c8cc
shashi0808 <96880312+shashi0808@users.noreply.github.com>
2022-06-28 21:34:00 +0530

    added candies and two sister codeforces

---
+++ candiesandtwosisterCodeforces.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ int t;
+ cin>>t;
+ while(t--){
+    int n,b;
+    cin>>n;
+    b=(n-1)/2;
+    cout<<b<<endl;
+	}
+ 
+	return 0;
+}
