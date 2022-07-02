41cd13b27de135c22a2bda844c2c7474ae9dfe72
shashi0808 <96880312+shashi0808@users.noreply.github.com>
2022-07-02 20:34:48 +0530

    added games Codeforces

---
+++ gamesCodeforces.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main() 
+{
+ int n;
+ int count=0;
+ cin>>n;
+ int a[n],h[n];
+ for(int i=0;i<n;i++)
+{
+    cin>>h[i]>>a[i];
+}
+for(int i=0;i<n;i++)
+{
+for(int j=0;j<n;j++)
+{
+    if(a[i]==h[j] )
+    {
+        count++;
+    }
+}
+}
+cout<<count;
+  return 0;
+}
