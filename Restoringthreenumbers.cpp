82fa182d05dec0f81eee55ef578667c24e61d671
shashi0808 <96880312+shashi0808@users.noreply.github.com>
2022-07-19 21:54:12 +0530

    added restoring three numbers Codeforces

---
+++ Restoringthreenumbers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	vector<int> v(4);
+	for(int i = 0; i < 4; i++)
+		cin >> v[i];
+	sort(v.begin(), v.end());
+	for(int i = 0; i < 3; i++)
+		cout << v[3] - v[i] << " ";
+	return 0;
+}
